0e2deb7b1f0e53fdd3e2f6c762cde82a0b382323
StanDimitroff <standimitroff@gmail.com>
2015-03-12 18:51:21 +0200

    3rd hw started

---
+++ 03. EF-Perfomance-Homework/AdsDbContext/01. ShowData.cs
@@ -0,0 +1,26 @@
+﻿namespace AdsDbContext
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ShowData
+    {
+        static void Main()
+        {
+            var adsWithoutUnclude = GetAdsWithoutInclude();
+            foreach (var ad in adsWithoutUnclude)
+            {
+                Console.WriteLine(ad.Title + " " + ad.AdStatus + " " + "  " + ad.Category + " " + ad.Town + " " + ad.AspNetUser);
+                Console.WriteLine();
+            }
+        }
+
+        private static List<Ad> GetAdsWithoutInclude()
+        {
+            var db = new AdsEntities();
+            return db.Ads.ToList();
+            
+        }
+    }
+}
